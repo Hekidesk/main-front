09807d455db977843bb01276b5a46c6d656284e3
perriex <parna80as@gmail.com>
2023-05-15 19:45:45 +0330

    remove login error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Home/Home.page";
-import LoginPage from "./pages/Login/Login.page";
 import RegisterPage from "./pages/RegisterUser/Register.page";
 import RegisterDevicePage from "./pages/RegisterDevice/RegisterDevice.page";
 
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={null}>
           <Route index element={<HomePage />} />
-          <Route path={"login"} element={<LoginPage />} />
           <Route path={"register-user"} element={<RegisterPage />} />
           <Route path={"register-device"} element={<RegisterDevicePage />} />
         </Route>
