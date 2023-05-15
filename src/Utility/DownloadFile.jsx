e4fa9ccf608277fe577bb27af72c61332a93693d
perriex <parna80as@gmail.com>
2023-05-15 21:49:04 +0330

    remove old code

--- src/Utility/DownloadFile.jsx
+++ src/Utility/DownloadFile.jsx
@@ -1,9 +1,9 @@
 import { jsPDF } from "jspdf";
 
 export function prepareURLFile(texts, extraChart = [], extraText = []) {
-  var oldCanvas = document.querySelector('#chartContainer canvas')
-  var newCanvas = document.createElement('canvas');
-  var context = newCanvas.getContext('2d');
+  const oldCanvas = document.querySelector('#chartContainer canvas')
+  const newCanvas = document.createElement('canvas');
+  const context = newCanvas.getContext('2d');
   
   //set dimensions
   newCanvas.width = oldCanvas.width;
@@ -11,7 +11,7 @@ export function prepareURLFile(texts, extraChart = [], extraText = []) {
   context.fillStyle = "white";
   context.fillRect(0, 0, newCanvas.width, newCanvas.height);
   context.drawImage(oldCanvas, 0, 0);
-  var yPosition = 50;
+  let yPosition = 50;
   texts.map((text) => {
     context.fillStyle = "black";
     context.font = "25px Comic Sans MS";
@@ -19,10 +19,10 @@ export function prepareURLFile(texts, extraChart = [], extraText = []) {
     yPosition += 50;
   });
 
-  extraChart.map((chart, i) => {
-    var oldChart = document.querySelector(chart)
+  extraChart.forEach((chart, i) => {
+    const oldChart = document.querySelector(chart)
     context.drawImage(oldChart, 0, (oldCanvas.height + 200)*(i+1));
-    var yPosition = 150;
+    const yPosition = 150;
     extraText[i].map((text) => {
       context.fillStyle = "black";
       context.font = "25px Comic Sans MS";
@@ -30,12 +30,12 @@ export function prepareURLFile(texts, extraChart = [], extraText = []) {
     });
   })
 
-  var dataURL = newCanvas.toDataURL("image/jpeg", 1.0);
+  const dataURL = newCanvas.toDataURL("image/jpeg", 1.0);
   return dataURL;
 }
 
 function downloadImage(data, filename = 'untitled.jpeg') {
-  var a = document.createElement('a');
+  const a = document.createElement('a');
   a.href = data;
   a.download = filename;
   document.body.appendChild(a);
@@ -43,13 +43,13 @@ function downloadImage(data, filename = 'untitled.jpeg') {
 }
 
 export function downloadSVGAsPNG(e, dataKey, texts) {
-  var dataURL = prepareURLFile(texts); 
+  const dataURL = prepareURLFile(texts); 
   const fileName = dataKey + ".png";
   downloadImage(dataURL, fileName);
 }
 
 export function downloadPDFAsPNG(e, dataKey, texts) {
-  var dataURL = prepareURLFile(texts); 
+  const dataURL = prepareURLFile(texts); 
   const fileName = dataKey + ".pdf";
   const doc = new jsPDF();
   doc.addImage(dataURL, "png", 0, 10, 200, 100).save(fileName);
