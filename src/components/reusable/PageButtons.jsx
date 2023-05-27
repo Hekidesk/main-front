5f0b2c83cf044778a59822dbeab8f2ae300f4b2f
perriex <parna80as@gmail.com>
2023-05-27 14:09:00 +0330

    button fixed

--- src/components/reusable/PageButtons.jsx
+++ src/components/reusable/PageButtons.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+  import React from 'react'
 import { Image, Row, Col, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import {ButtonMeasurementStyle, ButtonBackStyle} from "@/components/reusable/ButtonStyle";
