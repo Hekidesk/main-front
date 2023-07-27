9ee9c6a9d4de9b771df5a6958ccafbfcda1a3427
perriex <parna80as@gmail.com>
2023-07-27 14:50:20 +0330

    refactor measurement page

---
+++ src/pages/Measurement/components/CSS.jsx
@@ -0,0 +1,7 @@
+export const CardStyle = {
+  marginBottom: "30px",
+  backgroundColor: "var(--dirty-white)",
+  border: "none",
+  height: "max-content",
+  padding: "1em",
+};
--- src/pages/Measurement/components/Measurement.jsx
+++ src/pages/Measurement/components/Measurement.jsx
@@ -1,14 +1,23 @@
-import { Col, Row, Card } from "react-bootstrap";
+// Third party
 import { Link } from "react-router-dom";
 
+// PRIME REACT
+import { Card } from "primereact/card";
+
+// HEKIDESK
+import { ButtonMyDeskStyle } from "HEKIDESK/components/reusable/ButtonStyle";
+import { Title2 } from "HEKIDESK/components/reusable/Title";
+
+// SVG
+import MyDeskIcon from "HEKIDESK/assets/icon/myDesk.svg";
 import CarIcon from "HEKIDESK/assets/icon/measurement/cardiogramIcon.svg";
 import OxIcon from "HEKIDESK/assets/icon/measurement/oximetryIcon.svg";
 import SoundIcon from "HEKIDESK/assets/icon/measurement/heartLungSoundIcon.svg";
 import TemeperatureIcon from "HEKIDESK/assets/icon/measurement/temeperatureIcon.svg";
 import BpIcon from "HEKIDESK/assets/icon/measurement/bloodPressureIcon.svg";
-import "HEKIDESK/assets/styles/Measurement.css";
-import { ButtonMyDeskStyle } from "HEKIDESK/components/reusable/ButtonStyle";
-import MyDeskIcon from "HEKIDESK/assets/icon/myDesk.svg";
+
+// Style
+import { CardStyle } from "./CSS";
 
 const Measurement = () => {
   const routes = [
@@ -24,51 +33,50 @@ const Measurement = () => {
   ];
   return (
     <div>
-      <Row>
-        <Row>
-          <h2 className="title-name">Measurement</h2>
-        </Row>
-        <Row style={{ minHeight: "40vh" }}>
+      <div className="d-flex flex-column">
+        <div className="d-flex flex-column my-2">
+          <Title2>Measurement</Title2>
+        </div>
+        <div className="row">
           {routes.map((item, i) => (
-            <Col lg={4} sm={6} key={i}>
+            <div className="col-4" key={i}>
               <Link
                 to={
                   // eslint-disable-next-line no-undef
                   process.env.REACT_APP_BASE_URL + "/measurement" + item.route
                 }
               >
-                <Card>
-                  <Card.Body>
-                    <Card.Title>
-                      <img
-                        src={item.icon}
-                        style={{ height: "2em", width: "auto" }}
-                      />
-                    </Card.Title>
-                    <div className="record-text">measure of</div>
-                    <Card.Text>{item.name}</Card.Text>
-                  </Card.Body>
+                <Card
+                  header={
+                    <img
+                      src={item.icon}
+                      style={{
+                        height: "2.5em",
+                        width: "auto",
+                        paddingLeft: "20px",
+                      }}
+                    />
+                  }
+                  style={CardStyle}
+                >
+                  <dispatchEvent>measure of</dispatchEvent>
+                  <h4 className="fw-bold">{item.name}</h4>
                 </Card>
               </Link>
-            </Col>
+            </div>
           ))}
-        </Row>
-      </Row>
-      <Row className="d-flex justify-content-end">
+        </div>
+      </div>
+      <div className="d-flex justify-content-end">
         <Link
           // eslint-disable-next-line no-undef
           to={process.env.REACT_APP_BASE_URL + "/user-desk"}
           style={ButtonMyDeskStyle}
         >
-          <img
-            src={MyDeskIcon}
-            alt="Image"
-            width="16px"
-            style={{ margin: "0em 0.2em" }}
-          />
-          My Desk
+          <img src={MyDeskIcon} alt="Image" width="16px" className={"mx-2"} />
+          {" My Desk"}
         </Link>
-      </Row>
+      </div>
     </div>
   );
 };
